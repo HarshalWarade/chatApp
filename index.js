5c38bdfc200b66d173a07cec70987102d01c7d80
harshalDev <harshalwarade344@gmail.com>
2022-12-02 22:03:50 +0530

    Changes are saving...

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, { cors: { origin: "*" } });
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'config.env' });
+const port = process.env.PORT;
+
+app.use(express.urlencoded());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+server.listen(port, (err) => {
+    console.log('connected')
+});
+io.on('connection', (socket) => {
+    socket.on("message", (data) => {
+        socket.broadcast.emit('message', data)
+    })
+})
